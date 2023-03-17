29407f246b21c028ed854d293aa11dc06af90fc4
Ryan Smith <smithr38@wit.edu>
2023-03-17 01:18:51 -0400

    rewrote selectors for updated html/css

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,13 @@
 const api = "http://127.0.0.1:8000";
 
 async function getCalendar() {
+    // Fetch calendar data from API
     const response = await fetch(api + "/calendar", { method: "GET" });
     console.debug(`GET ${api}/calendar: `)
     const data = await response.json();
     console.debug(data);
 
+    // Sort races into completed (races ends before current date) and upcoming (race ends after current date)
     let completed_races = []; upcoming_races = [];
     data.forEach(race => {
         let end_dt = Date.parse(race["events"][0].end_time_local);
@@ -17,23 +19,31 @@ async function getCalendar() {
             upcoming_races.push(race);
         }
     });
+    // Next race is first in upcoming races list
     let next_race = upcoming_races[0]
     console.debug("Next race: ", next_race);
 
+    // Set location and race date elements to data
+    const dateOptions = { weekday: 'short', month: 'short', day: 'numeric' }
+    const timeOptions = { hour: 'numeric', minute: 'numeric' }
     const start = new Date(next_race["events"][0]["start_time_local"]);
-    const end = new Date(next_race["events"][0]["end_time_local"]);
-    document.querySelector(".location").textContent = next_race["location"];
-    document.querySelector(".date-time span:nth-child(1)").textContent = start.toDateString();
-    document.querySelector(".date-time span:nth-child(2)").textContent = start.toLocaleTimeString();
-    document.querySelector(".date-time span:nth-child(3)").textContent = end.toLocaleTimeString();
+    document.querySelector(".country").textContent = next_race["location"];
+    document.querySelector(".date").textContent = start.toDateString(undefined, dateOptions);
+    document.querySelector(".time").textContent = start.toLocaleTimeString(undefined, timeOptions);
 
+    // Set elements for each event to data
     for (let i = 1; i < next_race["events"].length; i++) {
         let event = next_race["events"][i];
         let event_start = new Date(event["start_time_local"]);
-        let event_end = new Date(event["end_time_local"]);
-        document.querySelector(`#event${i} h4`).textContent = event["event"];
-        document.querySelector(`#event${i} span:nth-of-type(1)`).textContent = event_start.toDateString();
-        document.querySelector(`#event${i} span:nth-of-type(2)`).textContent = event_start.toLocaleTimeString();
-        document.querySelector(`#event${i} span:nth-of-type(3)`).textContent = event_end.toLocaleTimeString();
+        document.querySelector(`.event:nth-child(${i}) .event-name`).textContent = event["event"];
+        document.querySelector(`.event:nth-child(${i}) .event-date`).textContent = event_start.toDateString(undefined, dateOptions);
+        document.querySelector(`.event:nth-child(${i}) .event-time`).textContent = event_start.toLocaleTimeString(undefined, timeOptions);
     }
+}
+
+function onLoad() {
+    getCalendar()
+
+    // Select first tab as default
+    document.querySelector("nav a:first-child").click()
 }
