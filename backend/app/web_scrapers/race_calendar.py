346309415d28064838a18654d9ad9c344d56ecef
Ryan Smith <smithr38@wit.edu>
2024-12-10 18:49:55 -0500

    switch calendar api to pitwall.app due to changes in f1.com

--- backend/app/web_scrapers/race_calendar.py
+++ backend/app/web_scrapers/race_calendar.py
@@ -1,30 +1,47 @@
+import re
 from datetime import datetime as dt
+from datetime import timedelta
 
 import bs4
+import pytz
 import requests
 from app.model import Event, Race
 from bs4 import BeautifulSoup
+from bs4.element import NavigableString, Tag
 
-url = f"https://www.formula1.com/en/racing/{dt.today().year}.html"
+url = "https://pitwall.app/"
 
 
 # Get info for all races for the year
 def calendar() -> list[Race]:
-    page = requests.get(url)  # Request HTML page
-    soup = BeautifulSoup(page.content, "html.parser")  # Parse HTML with beautiful soup
-    link_elements = soup.select(
-        "a[data-roundtext]"
-    )  # use CSS selector to find all link elements with attribute 'data-roundtext'
+    page = requests.get(url + "races")
+    soup = BeautifulSoup(page.content, "html.parser")
 
     races = []
-    # get data for each race
-    for link in link_elements:
+    table = soup.find("tbody")
+    for row in table.children:
+        if type(row) is NavigableString:
+            continue
+        race_url = row.find_next("a")["href"]
+        race_page = requests.get(url + race_url)
+        race_soup = BeautifulSoup(race_page.content, "html.parser")
+
         data = {}
-        data["round"] = link["data-roundtext"]
-        data["location"] = link["data-racecountryname"]
-        events, circuit = get_events_info("https://www.formula1.com" + link["href"])
-        data["circuit"] = circuit
-        data["events"] = events
+        info_pane = race_soup.find("div", class_="info-pane-data")
+
+        # Round (ex. 1/24)
+        round = info_pane.find("div", text=re.compile("Round")).find_next_sibling().text
+        data["round"] = round.split("/")[0]
+
+        # Location
+        data["location"] = info_pane.find("div", text=re.compile("Location")).find_next_sibling().text
+
+        # Circuit
+        data["circuit"] = info_pane.find("div", text=re.compile("Circuit")).find_next("a").text
+
+        # Events
+        timetable = race_soup.find("h3", text=re.compile("Timetable")).find_next("tbody")
+        data["events"] = get_events_info(timetable)
 
         race = Race(**data)
         races.append(race)
@@ -33,33 +50,39 @@ def calendar() -> list[Race]:
 
 
 # Find and parse race info
-def get_events_info(url: str) -> tuple[list[Event], str]:
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    time_table = soup.find("div", class_="f1-race-hub--timetable-listings")  # find first div with given class name
-
+def get_events_info(timetable: Tag) -> list[Event]:
     events = []
-    # pull data from all child tags of timetable div
-    for event in time_table.children:
-        if type(event) is bs4.element.Tag:
-            data = {}
-            data["event"] = soup.select(f".{event['class'][1]} p")[1].text
+    for event in timetable.children:
+        if type(event) is NavigableString or len(event.contents) != 7:
+            continue
 
-            start_time = dt.strptime(
-                event["data-start-time"] + event["data-gmt-offset"], "%Y-%m-%dT%H:%M:%S%z"
-            )  # ex) '2023-03-04T18:00:00' + '+03:00'
-            data["start_time_track"] = start_time
-            data["start_time_local"] = start_time.astimezone()
+        data = {}
 
-            end_time = dt.strptime(event["data-end-time"] + event["data-gmt-offset"], "%Y-%m-%dT%H:%M:%S%z")
-            data["end_time_track"] = end_time
-            data["end_time_local"] = end_time.astimezone()
+        # Event title
+        data["event"] = event.contents[1].text.strip()
 
-            event_data = Event(**data)
-            events.append(event_data)
+        # Start time
+        date_str = event.contents[3].text.strip()
+        time_str, tz_str = event.contents[5].text.strip().split(" ")
+        tz_str = "Europe/Berlin" if tz_str == "CEST" else tz_str
+        start_time = dt.strptime(f"{date_str} {time_str}", "%A %B %d, %Y %H:%M")
+        try:
+            start_time = pytz.timezone(tz_str).localize(start_time)
+        except pytz.exceptions.UnknownTimeZoneError:
+            print(f"Skipping localization for tz {tz_str}")
+        start_time_localized = start_time.astimezone()
+        data["start_time_track"] = start_time
+        data["start_time_local"] = start_time_localized
 
-    circuit = soup.select(".f1-race-hub--timetable-links-wrapper p")[0].text
-    return (events, circuit)
+        # End time
+        duration = 2 if data["event"] == "Race" else 1
+        data["end_time_track"] = start_time + timedelta(hours=duration)
+        data["end_time_local"] = start_time_localized + timedelta(hours=duration)
+
+        event_data = Event(**data)
+        events.append(event_data)
+
+    return events[::-1]
 
 
 if __name__ == "__main__":
